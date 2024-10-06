d3b2c30cadfbce2d37265ea8b548bbb3529f2f49
amitkothiyalutknd <aamit_kkothiyal@yahoo.com>
2024-10-07 02:59:43 +0530

    CICD Pipeline Project Excersice With Maven Demo Project.

--- Factorial.java
+++ Factorial.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Enter The Number For Factorial: ");
-        int n = scn.nextInt();
+        // Scanner scn = new Scanner(System.in);
+        // System.out.print("Enter The Number For Factorial: ");
+        // int n = scn.nextInt();
         int output = fact(4);
         System.out.print("The Factorial Output Is: ");
         System.out.print(output);
--- xPowerofn.java
+++ xPowerofn.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class xPowerofn {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Enter The Number: ");
-        int x = scn.nextInt();
-        System.out.print("Enter The Power Of Number: ");
-        int n = scn.nextInt();
+        // Scanner scn = new Scanner(System.in);
+        // System.out.print("Enter The Number: ");
+        // int x = scn.nextInt();
+        // System.out.print("Enter The Power Of Number: ");
+        // int n = scn.nextInt();
         int output = power(2, 4);
         System.out.print("The Output Is: ");
         System.out.print(output);
